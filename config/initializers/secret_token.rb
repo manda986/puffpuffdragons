6d62b2ab73b83da6eec8488b61d19684775008d7
Amanda Li <manda986@gmail.com>
2013-09-18 14:41:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puffpuffdragons::Application.config.secret_key_base = 'fbae102098a362f9c5cd1f932cd50f61f75735ec5e947a577c510cd469d93449098e44abe1910e03515bf201b69f7fd9ddf2d1e0e9ce8c86587fa77f531f161d'
